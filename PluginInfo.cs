10ef14883885691b34afac31a63b3a456265a468
WGODNUMBER1 <spideybair@gmail.com>
2026-04-18 18:10:35 -0400

    Create Emote Wheel

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+namespace FortniteEmoteWheel
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal static class PluginInfo
+    {
+        public const string GUID = "com.goldentrophy.gorillatag.fortniteemotewheel";
+        public const string Name = "FortniteEmoteWheel";
+        public const string Version = "0.0.1";
+    }
+}
